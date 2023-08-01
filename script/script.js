306f33b736ce413e1b13e2ef2ebb2302740fc440
Ryan  Vitor <ryanvitor29@gmail.com>
2023-08-01 13:03:49 -0300

    atualizei o menu

--- script/script.js
+++ script/script.js
@@ -1,8 +1,35 @@
 
 const menu = document.querySelector('.menu');
 const navMenu = document.querySelector('.nav-menu');
+const barra1 = document.getElementById('barra1');
+const barra2 = document.getElementById('barra2');
+const barra3 = document.getElementById('barra3');
 
-menu.addEventListener('click', () => {
-    menu.classList.toggle('ativo');
-    navMenu.classList.toggle('ativo');
+function  ativarMenu() {
+    if (navMenu.style.display == '' ) {
+        navMenu.style.display = 'block';
+    } else {
+        navMenu.style.display = '';
+    }
+    console.log('click feito menu')
+}
+
+function animacao() {
+    if (barra1.style.transform == '' && barra2.style.opacity == '' && barra3.style.transform == '') {
+        barra1.style.transform = 'translateY(8px) rotate(45deg)';
+        barra2.style.opacity = '0';
+        barra3.style.transform = 'translateY(-8px) rotate(-45deg)';
+    } else {
+        barra1.style.transform = '';
+        barra2.style.opacity = '';
+        barra3.style.transform = '';
+
+    }
+}
+
+
+menu.addEventListener('click', () =>{
+    ativarMenu();
+
+    animacao();
 })
